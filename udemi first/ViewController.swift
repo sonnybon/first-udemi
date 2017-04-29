3d516f121501cb32b607f330ab26456a67b45fee
Sonny Bonyadi <sonny@sonnys-macbook-pro.local>
2017-04-29 18:17:18 -0400

    Initial Commit

---
+++ udemi first/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udemi first
+//
+//  Created by Sonny Bonyadi on 2017-04-29.
+//  Copyright © 2017 Sonny Bonyadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
